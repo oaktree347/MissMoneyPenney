f566d05ee6f106643ad0d279d413e2377adbe78d
Bren Briggs <briggs.brenton@gmail.com>
2016-11-06 18:15:09 -0800

    Upgrade to use new Legobot

--- chatbot.py
+++ chatbot.py
@@ -1,308 +1,40 @@
-import Legobot
-import ConfigParser
+import logging
+import threading
+import configparser
+from Legobot.Lego import Lego
+from Local.Roll import Roll
+from Local.CourageWolf import Encourage
 
-# bing_search() courtest of paraxor
-# TODO: fetch more results
-def bing_search(msg):
-    import random, urllib2, random, re
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
-    query = ' '.join(msg.splitMessage[4:])
-    query = query.replace(' ', '%20')
-    req_url = 'http://www.bing.com/images/search'
-    req_url += '?q=' + query
-    req_url += '&safesearch=on'
-    request = urllib2.Request(req_url)
-    request.add_header('User-Agent', user_agent)
-    request_opener = urllib2.build_opener()
-    response = request_opener.open(request)
-    response_data = response.read()
-    urls = []
-    for url in re.finditer('"https?://[^"]*\.(jpg|jpeg|png|gif|gifv)"', response_data):
-        urls += [url.group(0).strip('"')]
+from Legobot.Connectors.IRC import IRC
+from Legobot.Legos.Help import Help
 
-    if len(urls) == 0:
-        url = 'Nothing found :('
-    else:
-        url = random.choice(urls)
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-    return url
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(ch)
 
-def helloWorld(msg):
-    return "Hello, world!"
+# Initialize lock and baseplate
+lock = threading.Lock()
+baseplate = Lego.start(None, lock)
+baseplate_proxy = baseplate.proxy()
 
-def is_num(val):
-    try:
-        if float(val) != int(val):
-            return False
-
-        test = int(val)
-        test = int(float(val))
-
-        return True
-    except:
-        return False
-
-def wiki_search(msg):
-    import requests
-    baseurl = 'https://en.wikipedia.org/w/index.php?search='
-    search_params = '%20'.join(msg.splitMessage[4:])
-    r = requests.get(baseurl+search_params)
-    if r.status_code == 200:
-        return "I found this: " + r.url
-    else:
-        return "I could not reach Wikipedia. Sorry."
-
-def tip_user(msg):
-    #Initialize empty list of tipped users
-    import os
-
-    tipped = []
-    nick_to_tip = msg.arg1
-    amt_to_tip = msg.arg2
-
-    tips_file = ConfigParser.SafeConfigParser()
-    if os.path.isfile('tips.cfg'):
-        tips_file.read('tips.cfg')
-    else:
-        tips_file.add_section('Tips')
-
-    try:
-        for (key,value) in tips_file.items('Tips'):
-            tipped.append(key)
-    except Exception as e:
-        return e
-
-    if nick_to_tip is None or amt_to_tip is None:
-        #must have 2 args
-        return "wat?"
-
-    else:
-        #proper number of args
-        if nick_to_tip in tipped:
-            #user already tipped. get value, increment, write back to configparse obj
-            tipval = tips_file.get('Tips',nick_to_tip)
-            tipval = int(amt_to_tip) + int(tipval)
-            tips_file.set('Tips',nick_to_tip,str(tipval))
-        else:
-            tipval = amt_to_tip
-            tips_file.set('Tips',nick_to_tip,str(tipval))
-
-        tipval = tips_file.get('Tips',nick_to_tip)
-        returnVal = "%s tipped %s internet points, giving them a total of %s" % (nick_to_tip, str(amt_to_tip), tipval)
-    with open('tips.cfg','wb') as f:
-        tips_file.write(f)
-    return returnVal
-
-def print_tips(msg):
-    import os
-    import ConfigParser
-
-    tips_file = ConfigParser.SafeConfigParser()
-    if os.path.isfile('tips.cfg'):
-        tips_file.read('tips.cfg')
-    else:
-        return "It appears nobody has been tipped yet."
-
-    if msg.arg1:
-        try:
-            tipval = tips_file.get('Tips',msg.arg1)
-            returnval = "%s has %s meaningless internet points." % (msg.arg1, tipval)
-        except:
-            returnval = "I don't think %s has been tipped yet. Poor, pointless soul." % msg.arg1
-    else:
-        try:
-            tipped = []
-            for (key,value) in tips_file.items('Tips'):
-                tipped.append("%s: %s" % (key, value))
-        except Exception as e:
-            return e
-        tipped = ", ".join(tipped)
-        returnval = "Current tips: %s" % tipped
-    return returnval
-
-def check_weather_by_zip(msg):
-    # check_weather_by_zip
-    # Uses the Weather Underground API to check current conditions and a forecast
-    # Pulls its API key from api.cfg file in the same directory as your bot. 
-
-    import ConfigParser
-    import re
-    import requests
-    import os
-
-    if not msg.arg1:
-        return "Please provide a zip code for me to check"
-    else:
-        if re.match(r'^\d{5}$',msg.arg1) is not None:
-            pass
-        else:
-            return "I only support 5 digit, US zip codes."
-
-    api_keys = ConfigParser.SafeConfigParser()
-    if os.path.isfile('api.cfg'):
-        api_keys.read('api.cfg')
-    else:
-        return "No API keys found. Please initialize your api.cfg file."
-
-    try:
-        WUNDERGROUND_API_KEY = api_keys.get('API','wunderground')
-    except Exception as e:
-        return e
-
-
-    zipcode = msg.arg1
-    current = requests.get("http://api.wunderground.com/api/%s/conditions/q/%s.json" % (WUNDERGROUND_API_KEY, zipcode))
-    forecast = requests.get("http://api.wunderground.com/api/%s/forecast/q/%s.json" % (WUNDERGROUND_API_KEY, zipcode))
-    current = current.json()
-    forecast = forecast.json()
-    try: 
-        location = current['current_observation']['display_location']['full']
-        condition = current['current_observation']['weather']
-        temp_f = current['current_observation']['temp_f']
-        humidity = current['current_observation']['relative_humidity']
-        feelslike_f = current['current_observation']['feelslike_f']
-        wind_condition = current['current_observation']['wind_string']
-        wind_dir = current['current_observation']['wind_dir']
-        wind_speed = current['current_observation']['wind_mph']
-        wind_gust = current['current_observation']['wind_gust_mph']
-
-        short_forecast_period = forecast['forecast']['txt_forecast']['forecastday'][1]['title']
-        short_forecast_data = forecast['forecast']['txt_forecast']['forecastday'][1]['fcttext']
-
-        forecast_url = current['current_observation']['forecast_url']
-
-    except: 
-        return "Unable to find information on that zip code right now. Please check again later or petition Congress to have it created."
-
-    reply = "The weather in %s is currently %s with a temperature of %s degrees, humidity of %s, and it feels like %s degress. Wind is %s, blowing %s at %s mph with %s mph gusts. Forecast for %s: %s" % (
-        location,
-        condition,
-        temp_f,
-        humidity,
-        feelslike_f,
-        wind_condition,
-        wind_dir,
-        wind_speed,
-        wind_gust,
-        short_forecast_period,
-        short_forecast_data,
-        )
-    return reply
-
-def roll(msg):
-    import random
-    """
-    Inputs:
-      takes msg object
-
-    Outputs:
-      returns string to echo back to user in IRC
-
-    Purpose:
-      flip an imaginary coin or roll an imaginary x sided die
-      Usage:
-      !roll [# of sides]
-    """
-    num_dice = 1
-
-    if msg.arg2:
-        if not is_num(msg.arg2):
-            return "Incorrect number of dice. You must use a (sane) number."
-        else:
-            num_dice = int(msg.arg2)
-
-    if not msg.arg1:
-        # Behave like a coin
-        toss = random.randint(0,1)
-        if toss == 0:
-            returnVal = "Heads"
-        else:
-            returnVal = "Tails"
-    else:
-        if not is_num(msg.arg1):
-            returnVal = "Incorrect syntax. You must use a (sane) number"
-        elif is_num(msg.arg1) and not int(msg.arg1) >= 2:
-            returnVal = "No. Use two or more sides."
-        elif is_num(msg.arg1) and int(msg.arg1) == 2:
-            toss = random.randint(1,2)
-            if toss == 1:
-                returnVal = "Heads"
-            else:
-                returnVal = "Tails"
-        else:
-            results = []
-            for i in range(num_dice):
-                results.append(str(random.randint(1,int(msg.arg1))))
-                returnVal = "Your roll: %s" % " ".join(results)
-    return returnVal
-
-def xkcd(msg):
-    import urllib2, re
-    webpage = urllib2.urlopen('http://dynamic.xkcd.com/random/comic')
-    webpage = webpage.read()
-    print webpage
-    comic = re.search(r'<div id="comic".*?\n?.*?(//im.+?)".+?\s?title="(.+?)"',webpage)
-    if comic:
-        altText = comic.group(2).replace("&#39;","'")
-        returnVal = "%s %s" %(altText,"http:" + comic.group(1))
-        return returnVal
-
-
-def fraqdef(msg):
-    return 'hey fraq what is a ' + ' '.join(msg.splitMessage[4:])
-
-
-def main():
-    config = ConfigParser.SafeConfigParser()
-
-    config.read("chatbot.cfg")
-    HOST = config.get('Main','host')
-    PORT = config.getint('Main','port')
-    NICK = config.get('Main','nick')
-    HOSTPW = None
-
-    try:
-        HOSTPW = config.get('Main','hostpw')
-    except:
-        print "Host password not found, skipping..."
-
-    CHANS = []
-    for (key, val) in config.items('Channels'):
-        chan = None
-        chan_pass = None
-        val = val.strip()
-        val = val.split(' ')
-
-        if len(val) == 1:
-            chan = val[0]
-        if len(val) == 2:
-            chan = val[0]
-            chan_pass = val[1]
-
-        if chan_pass:
-            CHANS.append((chan,chan_pass))
-        else:
-            CHANS.append((chan,''))
-
-        if HOSTPW:
-            mybot = Legobot.legoBot(host=HOST,port=PORT,nick=NICK,nickpass="",chans=CHANS,hostpw=HOSTPW)
-        else:
-            mybot = Legobot.legoBot(host=HOST,port=PORT,nick=NICK,nickpass="",chans=CHANS)
-
-
-    mybot.addFunc("!helloworld", helloWorld, "Ask your bot to say hello. Usage: !helloworld")
-    mybot.addFunc("!img", bing_search, "Search Bing for a random image based on your input. Safe search is on, but you have been warned. Usage: !img [words to search]")
-    mybot.addFunc("!roll", roll, "Roll a magical N-sided die. Usage !roll [ N>1 sides ] [number of die to roll]")
-    mybot.addFunc("!xkcd", xkcd, "Pulls a random XKCD comic. Usage: !xkcd")
-    mybot.addFunc("!tip", tip_user, "Tip a specific user. Usage !tip [user]")
-    mybot.addFunc("!weather", check_weather_by_zip, "Check weather by zipcode. Usage: !weather 36429")
-    mybot.addFunc('!printtips', print_tips, "See who has been tipped")
-    mybot.addFunc('!wtf', wiki_search, "Wikipedia Top Finder. Grab a wikipedia article using a word or phrase as your search parameter. Usage: !wtf [parameter]")
-    mybot.addFunc('!fraqdef', fraqdef, "Fraqture Search. Ask fraq what something is. Usage !fraqdef [parameter]")
-    mybot.connect(isSSL=True)
-
-
-
-
-if __name__ == "__main__":
-    main()
+# Add children
+baseplate_proxy.add_child(IRC,
+                          channels=['#social'],
+                          nickname=config['sithmail']['username'],
+                          server=config['sithmail']['host'],
+                          port=int(config['sithmail']['port']),
+                          use_ssl=config.getboolean('sithmail','ssl'),
+                          username=config['sithmail']['username'],
+                          password=config['sithmail']['password'])
+baseplate_proxy.add_child(Help)
+baseplate_proxy.add_child(Roll)
+baseplate_proxy.add_child(Encourage,encouragement='encouragement.txt')
--- test_chatbot.py
+++
@@ -1,6 +0,0 @@
-from chatbot import *
-from Legobot import Message
-
-def test_wiki_search():
-    msg = Message('!wtf United States')
-    assert wiki_search(msg) == 'I found this: https://en.wikipedia.org/wiki/United_States'
