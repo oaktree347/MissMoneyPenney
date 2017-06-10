b45692d1d64c660b292c885ca4c052b06003c0f4
Bren Briggs <briggs.brenton@gmail.com>
2017-06-10 00:51:44 -0400

    my hands are typing worrrrrrds

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it --name $1 -v "$PWD":/usr/src/legobot -w /usr/src/legobot python:3 sh -c 'pip install -r requirements.txt && python chatbot.py'
+docker run -d -it --net=host --name $1 -v "$PWD":/usr/src/legobot -w /usr/src/legobot legobot
